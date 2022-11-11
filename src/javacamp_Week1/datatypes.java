b8431002dd1245bbcca400a32bd95a8b09eccccc
MUSTAFA SAV <mustafasav@gmail.com>
2022-11-11 23:26:57 +0300

    11.11.2022

---
+++ src/javacamp_Week1/datatypes.java
@@ -0,0 +1,22 @@
+package javacamp_Week1;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		System.out.println("Veri Tipleri");
+		int number = 25;
+		number = +15;
+
+		byte number1 = 127; // byte: -128<>127 arasında
+
+		double ondalikSayi = 12.5;
+
+		float ondalikSayi2 = 12.5f;
+
+		char karakter = 'A';// Tek karakter
+
+		boolean durumNedir = true;
+
+	}
+
+}
