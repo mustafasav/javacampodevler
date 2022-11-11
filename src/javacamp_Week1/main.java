ce93a3b9e01604ebeb9d593d9a83440fe060adbf
MUSTAFA SAV <mustafasav@gmail.com>
2022-11-11 23:38:38 +0300

    11.11.2022

--- src/javacamp_Week1/main.java
+++ src/javacamp_Week1/main.java
@@ -3,9 +3,13 @@ package javacamp_Week1;
 public class main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Merhaba Java!!!");
-		
+		/*
+		 * https://www.youtube.com/watch?v=a8Fe2qbnYDM&list=
+		 * PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8&index=2 Bu oynatma listesinde 1-23
+		 * arasındaki tüm dersleri izleyip uygulayınız.(1-23 dahil)
+		 */
 
 	}
 
